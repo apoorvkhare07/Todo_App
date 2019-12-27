76a81a49468df28fc139eb58929bd7a16c1772b9
apoorv <apoorvkhare007@gmail.com>
2019-12-27 12:45:14 +0530

    todo app functionality added

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  todoArray = [];
+
+  addTodo(value){
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+
+  removeTodo(value){
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+      if(value== this.todoArray[i]){
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
 }
